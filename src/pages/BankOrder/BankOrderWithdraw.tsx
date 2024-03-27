67178ad2ca7be69b69c2f9c33809f39c05cbe22d
gr0nn1x <fanyfany888@gmail.com>
2024-03-27 08:12:47 +0100

    ifelseifelseifelseifelse

--- src/pages/BankOrder/BankOrderWithdraw.tsx
+++ src/pages/BankOrder/BankOrderWithdraw.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { remove } from "./BankSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { BankType } from "./BankSlice";
 
 export default function BankOrderWithdraw() {
+  const count = useSelector((state: BankType) => state.bank.counter);
   const dispatch = useDispatch();
   const [value, setValue] = useState(0);
   const sumbit = (e: React.FormEvent) => {
-    dispatch(remove(value));
+    if (value <= count) {
+      dispatch(remove(value));
+    }
   };
 
   const change = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +24,12 @@ export default function BankOrderWithdraw() {
         <h3>Zpět</h3>
       </Link>
       <form>
-        <input type="number" placeholder="Zadejte částku" onChange={change}className="maininput" />
+        <input
+          type="number"
+          placeholder="Zadejte částku"
+          onChange={change}
+          className="maininput"
+        />
       </form>
       <Link to={""}>
         <input type="submit" value="Vybrat" onClick={sumbit}></input>
